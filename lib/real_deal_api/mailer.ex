8b5e1c26b6f298eacd9fe25fd892006fa725ec55
abey onalaja <abey.onalaja@gmail.com>
2023-05-13 17:25:27 +0100

    project so far

---
+++ lib/real_deal_api/mailer.ex
@@ -0,0 +1,3 @@
+defmodule RealDealApi.Mailer do
+  use Swoosh.Mailer, otp_app: :real_deal_api
+end
---
+++ lib/real_deal_api_web/router.ex
@@ -0,0 +1,54 @@
+defmodule RealDealApiWeb.Router do
+  use RealDealApiWeb, :router
+  use Plug.ErrorHandler
+
+  defp handle_errors(conn, %{reason: %Phoenix.Router.NoRouteError{message: message}}) do
+    conn
+    |> json(%{errors: message})
+    |> halt()
+  end
+
+  defp handle_errors(conn, %{reason: %{message: message}}) do
+    conn
+    |> json(%{errors: message})
+    |> halt()
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+    plug :fetch_session
+  end
+
+  pipeline :auth do
+    plug RealDealApiWeb.Auth.Pipeline
+    plug RealDealApiWeb.Auth.SetAccount
+  end
+
+  scope "/api", RealDealApiWeb do
+    pipe_through :api
+    post "/accounts/create", AccountController, :create
+    post "/accounts/sign_in", AccountController, :sign_in
+  end
+
+  scope "/api", RealDealApiWeb do
+    pipe_through [:api, :auth]
+    get "/accounts/by_id/:id", AccountController, :show
+  end
+
+  # Enable LiveDashboard and Swoosh mailbox preview in development
+  if Application.compile_env(:real_deal_api, :dev_routes) do
+    # If you want to use the LiveDashboard in production, you should put
+    # it behind authentication and allow only admins to access it.
+    # If your application does not have an admins-only section yet,
+    # you can use Plug.BasicAuth to set up some basic authentication
+    # as long as you are also using SSL (which you should anyway).
+    import Phoenix.LiveDashboard.Router
+
+    scope "/dev" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+
+      live_dashboard "/dashboard", metrics: RealDealApiWeb.Telemetry
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
